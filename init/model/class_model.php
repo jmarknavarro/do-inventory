7b8484b68dfe7e8ac79180b2da4c2a8ffab47119
John Mark Navarro <90978126+jmarknavarro@users.noreply.github.com>
2023-03-27 10:56:46 +0800

    Update class_model.php

--- init/model/class_model.php
+++ init/model/class_model.php
@@ -46,7 +46,7 @@ class class_model
     {
         try {
             $db = DB();
-            $sql = "SELECT `dept_id`,`office_id`,`category_id`,`user_name`,`serial_no`,`product_name`,`year_issued`,`warranty_status`,`status`,`remarks` FROM `tbl_product` WHERE id = :id";
+            $sql = "SELECT `dept_id`,`office_id`,`category_id`,`user_name`,`serial_no`,`product_name`,`year_issued`,`warranty_status`,`status`,`remarks` FROM `tbl_product` WHERE serial_no = :id";
             $query = $db->prepare($sql);
             $query->bindParam("id", $id, PDO::PARAM_STR);
             $query->execute();
@@ -97,7 +97,7 @@ class class_model
 					echo "<td>$row[status]</td>";
 					echo "<td>$row[remarks]</td>";
 					echo "<td>
-					<a class='btn text-center btn-sm' href='edit-record.php?id=$row[serial_no]' data-id='$row[id]'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+					<a class='btn text-center btn-sm' href='edit-record.php?id=$row[serial_no]'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
 					<path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
 					<path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>
 				  </svg></a>
