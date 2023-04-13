f433a324cfe4673e4ff122b4daee4c63c9f1b63d
John Mark Navarro <90978126+jmarknavarro@users.noreply.github.com>
2023-04-13 09:34:16 +0800

    Update delete_process.php

--- init/controllers/delete_process.php
+++ init/controllers/delete_process.php
@@ -1,12 +1,13 @@
 <?php
+		require_once "../model/class_model.php";;
 
 if (isset($_POST)) {
     $conn = new class_model();
-$del = $conn->deleteRecord($_POST['id']);
+    $del = $conn->deleteRecord($_POST['id']);
+    if(!$del == TRUE){
+        echo '<script> setTimeout(function() {  window.history.go(-0); }, 1000); </script>';
 
+      }else{
+        echo '<script> setTimeout(function() {  window.history.go(-0); }, 1000); </script>';
     }
-
-
-
-
-?>
+}
